0a4f516fe782ccf37e3b5425e26ea17bc8df60fb
Prachalit Parate <prachalitparate13@gmail.com>
2024-10-15 22:24:39 +0530

    first commit

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -14,7 +14,7 @@ function Header() {
   };
   return (
     <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-[400px] p-4">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between lg:mx-[400px] sm:mx-20 p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
             src="/logo.jpg"
